5434736407fa7efc722e2b6ea31ed7013bfc8aea
Odigene Joseph <Odigene12@gmail.com>
2016-09-19 20:03:54 -0500

    Reinstalled configurations and created first unit test to check that model is not null

---
+++ SavingVariables.Tests/DAL/VariableTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Data.Entity;
+using SavingVariables.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SavingVariables.Tests.DAL
+{
+    [TestClass]
+    public class VariableTests
+    {
+        //Test to make sure a new instance of the DbSet is not null.
+        [TestMethod]
+        public void CanIMakeAnInstanceOfVariableClass()
+        {
+            Mock<DbSet<Variables>> newvariables = new Mock<DbSet<Variables>>();
+            Assert.IsNotNull(newvariables);
+        }
+    }
+}
---
+++ SavingVariables/Models/Variables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavingVariables.Models
+{
+    public class Variables
+    {
+        //This is the primary key for the Model
+        [Key]
+        public int VariableId { get; set; }
+
+        //The variable is going to be a character since variables are not words.
+        public char Variable { get; set; }
+
+        //This is the value of the variable as an integer.
+        public int Value { get; set; }
+    }
+}
